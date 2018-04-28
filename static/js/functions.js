1d66f88a9736cd1b2db7fc99e6c1d5cc3d9d9337
Zach Burke <madutilitybelt@gmail.com>
2018-04-28 01:31:25 -0500

    fixing carousel text size on ipad

--- static/js/functions.js
+++ static/js/functions.js
@@ -70,7 +70,7 @@
                     items: 1
                 },
                 768: {
-                    items: 3
+                    items: 2
                 },
                 992: {
                     items: 3
