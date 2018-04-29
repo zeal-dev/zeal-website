bc2d856059d8f95ef8e168d0a103fdba6557b401
Zach Burke <madutilitybelt@gmail.com>
2018-04-29 00:43:15 -0500

    setting autoplay back to true

--- static/js/functions.js
+++ static/js/functions.js
@@ -54,7 +54,7 @@
 	            '<i class="pe-7s-angle-right">'
 	        ],
             dots: false,
-			autoplay: false,
+			autoplay: true,
 		    autoplayTimeout: 5000,
 		    autoplayHoverPause: true,
             fluidSpeed: 1000,
