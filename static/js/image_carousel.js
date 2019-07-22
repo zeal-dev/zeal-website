972d05324799008464f06ff0ecfd029a8354f163
Long Nguyen <18nguyenl@gmail.com>
2019-07-22 17:55:21 -0500

    implement desktop carousel functionality

--- static/js/image_carousel.js
+++ static/js/image_carousel.js
@@ -2,17 +2,20 @@ var bullets = document.querySelectorAll(".bullet");
 var slides = document.querySelectorAll(".slide");
 
 var _loop = function _loop(i) {
-	bullets[i].onclick = function () {
+	function slideReveal() {
 		var x = 0;
 		var y = 0;
 		while (x < bullets.length && y < slides.length) {
 			bullets[x++].className = 'bullet';
 			slides[y++].className = 'slide';
 		}
-
+	
 		bullets[i].className = "bullet bullet-active";
 		slides[i].className = "slide slide-active";
 	};
+
+	bullets[i].onmouseover = slideReveal;
+	bullets[i].onclick = slideReveal; // for mobile interaction
 };
 
 for (var i = 0; i < bullets.length; i++) {
