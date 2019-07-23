1f74745b63b72c1e50f9c0f596dc21f3c12857db
Long Nguyen <18nguyenl@gmail.com>
2019-07-23 14:54:56 -0500

    make carousel responsive

--- static/js/image_carousel.js
+++ static/js/image_carousel.js
@@ -1,23 +1,63 @@
-var bullets = document.querySelectorAll(".bullet");
-var slides = document.querySelectorAll(".slide");
 
-var _loop = function _loop(i) {
-	function slideReveal() {
-		var x = 0;
-		var y = 0;
-		while (x < bullets.length && y < slides.length) {
-			bullets[x++].className = 'bullet';
-			slides[y++].className = 'slide';
+document.addEventListener("DOMContentLoaded", (event) => {
+	const bullets = document.querySelectorAll(".bullet");
+	const sliderBullets = document.querySelector(".slider-bullets");
+	const slides = document.querySelectorAll(".slide");
+	const sliderNavigation = document.querySelectorAll(".slider-navigation");
+	const sliderBulletsLength = bullets[0].clientWidth * bullets.length;
+	
+	bullets[0].classList.add('bullet-active')
+	slides[0].classList.add('slide-active')
+	
+	let selected = [document.querySelector('.bullet-active'), document.querySelector('.slide-active')];
+
+	function ImageCarousel() {
+		if (sliderBulletsLength > sliderBullets.clientWidth) {
+			if (window.innerWidth < 576) {
+				// Hide the bullets until there are 1 left
+				for(let i = 1; i < bullets.length; i++) {
+					if (i >= 1)
+						bullets[i].classList.add("bullet-hide")
+					else
+						bullets[i].classList.remove("bullet-hide")
+				}
+			}
+			else if (window.innerWidth < 1440) {
+				// Hide the bullets until there are 3 left
+				for(let i = 0; i < bullets.length; i++) {
+					if (i >= bullets.length - 2) 
+						bullets[i].classList.add("bullet-hide")
+					else
+						bullets[i].classList.remove("bullet-hide")
+				}
+			}
+	
+		} else {
+			for(let i = 0; i < bullets.length; i++) {
+				bullets[i].classList.remove("bullet-hide")
+			}
 		}
 	
-		bullets[i].className = "bullet bullet-active";
-		slides[i].className = "slide slide-active";
-	};
-
-	bullets[i].onmouseover = slideReveal;
-	bullets[i].onclick = slideReveal; // for mobile interaction
-};
-
-for (var i = 0; i < bullets.length; i++) {
-	_loop(i);
-}
+		let _loop = function _loop(i) {
+			function slideReveal() {
+				selected[0].classList.remove('bullet-active')
+				selected[1].classList.remove('slide-active')
+				
+				bullets[i].className = "bullet bullet-active";
+				slides[i].className = "slide slide-active";
+	
+				selected = [bullets[i], slides[i]]
+			};
+			
+			bullets[i].onmouseover = slideReveal;
+			bullets[i].onclick = slideReveal; // for mobile interaction
+		};
+		
+		for (let i = 0; i < bullets.length; i++) {
+			_loop(i);
+		}
+	}
+	
+	window.addEventListener("load", ImageCarousel)
+	window.addEventListener("resize", ImageCarousel)
+})
